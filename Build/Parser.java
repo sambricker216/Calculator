261d91cc048730c3cac781a5fa7f065be3f731ad
Samuel Bricker <94862877+sambricker216@users.noreply.github.com>
2022-04-27 13:28:28 -0400

    Worked on add and mult for parser

--- Build/Parser.java
+++ Build/Parser.java
@@ -15,24 +15,63 @@ public class Parser {
     }
 
     private Expr abs(){
-        if(tokenList.get(index).getOp() == Ops.ABS){
+        if(index >= tokenList.size())
+            return null;
+        else if(tokenList.get(index).getOp() == Ops.ABS){
             index++;
             Expr absExpr = add();
-            if(index >= tokenList.size() && tokenList.get(index).getOp() !=  Ops.ABS)
+            if(index >= tokenList.size() || tokenList.get(index).getOp() !=  Ops.ABS)
                 return null;
-            else
+            else{
+                index++;
                 return absExpr;
+            }
         }
         else
             return add();
     }
 
     private Expr add(){
-        return null;
+        if(index >= tokenList.size())
+            return null;
+
+        Expr left = mult();
+        Expr right = null;
+
+        while(index <= tokenList.size() && (tokenList.get(index).getOp() == Ops.ADD || tokenList.get(index).getOp() == Ops.ADD)){
+            index++;
+            right = mult();
+            left = new BinExpr(left, right, tokenList.get(index).getOp());
+        }
+
+        if(right != null){
+            return new BinExpr(left, right, tokenList.get(index).getOp());
+        }
+        else{
+            return left;
+        }
+
     }
 
     private Expr mult(){
-        return null;
+        if(index >= tokenList.size())
+            return null;
+
+        Expr left = base();
+        Expr right = null;
+
+        while(index <= tokenList.size() && (tokenList.get(index).getOp() == Ops.MULT || tokenList.get(index).getOp() == Ops.DIV || tokenList.get(index).getOp() == Ops.MOD)){
+            index++;
+            right = base();
+            left = new BinExpr(left, right, tokenList.get(index).getOp());
+        }
+
+        if(right != null){
+            return new BinExpr(left, right, tokenList.get(index).getOp());
+        }
+        else{
+            return left;
+        }
     }
 
     private Expr base(){
