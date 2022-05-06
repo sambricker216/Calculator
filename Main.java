0feb64ddebddf076ff25eb3139e2edbb4cd1e422
Samuel Bricker <94862877+sambricker216@users.noreply.github.com>
2022-05-05 23:59:14 -0400

    Began working on main class

--- Main.java
+++ Main.java
@@ -1,4 +1,12 @@
-public class Main {
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Main extends JFrame{
+    public Main(){
+
+    }
+
     public static void main(String[] args) {
         
     }
