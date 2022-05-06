6a0ba001b5fd07f7a0f0f44111738ff63fe0bf87
Samuel Bricker <94862877+sambricker216@users.noreply.github.com>
2022-05-06 14:07:02 -0400

    Additional UI Components

--- Main.java
+++ Main.java
@@ -3,11 +3,36 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Main extends JFrame{
-    public Main(){
+    JPanel panel;
+    JLabel output;
+    JTextField calcEntry;
+    JTextField varEntry;
 
+    public Main(){
+        super("Calculator");
+        setLocationRelativeTo(null);
+        
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(3,2));
+
+        calcEntry = new JTextField(15);
+        varEntry = new JTextField(5);
+        output = new JLabel("");
+
+        panel.add(new JLabel("Enter expression here"));
+        panel.add(calcEntry);
+        panel.add(new JLabel("Enter variable value here"));
+        panel.add(varEntry);
+        panel.add(new JButton("Calculate"));
+        panel.add(output);
+        
+        add(panel);
     }
 
     public static void main(String[] args) {
-        
+        Main m = new Main();
+        m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        m.setSize(450,150);
+        m.setVisible(true);
     }
 }
